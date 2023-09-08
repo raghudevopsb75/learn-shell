2db399e01f8c65f73fdedf7a6169854c3d287aaa
Raghu K <raghuk.vit@gmail.com>
2023-09-08 08:15:04 +0530

    Expense Scripts

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,6 +1,10 @@
+# If we assign a name to commands , then it is a function
+
 # Declare a function
 abc() {
   echo Hello World
+  echo Hello Universe
+  echo Hello Galaxy
 }
 
 # How to access a function
