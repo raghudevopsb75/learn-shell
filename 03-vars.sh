71d2d63b90b9cdb51b32be11f1ff3f3f078c051f
Raghu K <raghuk.vit@gmail.com>
2023-09-08 07:50:34 +0530

    Expense Scripts

---
+++ 03-vars.sh
@@ -0,0 +1,12 @@
+# If we name some data it is called as variable
+
+# Syntax to delcare variable
+# var_name=data
+
+a=10
+x=100
+y=true
+name=DevOps
+
+## How to access variable
+echo value of a = $a
