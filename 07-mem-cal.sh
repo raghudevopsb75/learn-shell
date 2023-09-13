521ae9524d0747a70f142627cbf41258cc34028b
Raghu K <raghuk.vit@gmail.com>
2023-09-13 08:38:31 +0530

    Expense Scripts

--- 07-mem-cal.sh
+++ 07-mem-cal.sh
@@ -1,5 +1,9 @@
 total=$(free  | grep Mem | awk '{print $2}')
 free=$(free  | grep Mem | awk '{print $4}')
 
-free_percentage=$(echo "$free / $total * 100" | bc -l  | awk -F . '{print $1}')
-echo $free_percentage
+used_percentage=$(echo "100 - ($free / $total * 100)" | bc -l  | awk -F . '{print $1}')
+echo $used_percentage
+
+# 0-50 used -> good
+# 51 - 70 - orange
+# >70 - red
