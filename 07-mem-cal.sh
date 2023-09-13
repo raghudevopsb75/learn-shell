55c6dbc59a2405845f97a19da0ddfa37f74be414
Raghu K <raghuk.vit@gmail.com>
2023-09-13 08:42:41 +0530

    Expense Scripts

--- 07-mem-cal.sh
+++ 07-mem-cal.sh
@@ -7,3 +7,12 @@ echo $used_percentage
 # 0-50 used -> good
 # 51 - 70 - orange
 # >70 - red
+
+if [ $used_percentage -lt 50 ]; then
+  echo Mem stat is GREEN
+elif [ $used_percentage -lt 70 ]; then
+  echo Mem stat is ORANGE
+else
+  echo Mem stat is RED
+fi
+
